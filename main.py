4ad9d4dfd96cc646cd61d3b723471a8a612bc6ba
Dest.Com <antoine.azevedo-da-silva@hetic.net>
2022-07-02 04:14:14 +0200

    test

--- main.py
+++ main.py
@@ -5,6 +5,17 @@ import flask
 from flask import request, jsonify
 
 from instascrape import *
+from selenium.webdriver import Chrome
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+chromeOptions = Options()
+chromeOptions.add_argument("--headless")
+chromeOptions.addArguments("--disable-gpu")
+chromeOptions.addArguments("--no-sandbox")
+chromeOptions.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+
+webdriver = Chrome(executable_path=os.environ.get(
+    "CHROMEDRIVER_PATH"), options=chromeOptions)
 
 load_dotenv()
 
@@ -31,10 +42,10 @@ def hashtag():
     else:
         return "Error: No id field provided. Please specify an id."
 
-    headers = {
-        "user-agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Mobile Safari/537.36 Edg/87.0.664.57",
-        "cookie": "sessionid="+os.getenv('SESSIONID')+";"
-    }
+    SESSION_ID = os.getenv("SESSIONID")
+
+    headers = {"user-agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Mobile Safari/537.36 Edg/87.0.664.57",
+               "cookie": "sessionid="+SESSIONID+";"}
 
     scrape_hashtag = Hashtag(
         'https://www.instagram.com/explore/tags/' + hashtag + '/')
@@ -42,7 +53,7 @@ def hashtag():
 
     results = []
 
-    for post in scrape_hashtag.get_recent_posts(20):
+    for post in scrape_hashtag.get_recent_posts(20, webdriver=webdriver):
         results.append(post.display_url)
 
     return jsonify(results)
